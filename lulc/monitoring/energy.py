e542fa37093514a15076594a4379ca23bc6d8c11
Maciej Adamiak <maciej.adamiak@heigit.org>
2023-08-08 13:38:19 +0000

    feat: enabled energy consumption tracking

---
+++ lulc/monitoring/energy.py
@@ -0,0 +1,48 @@
+import logging
+from tempfile import TemporaryDirectory
+
+from neptune import Run
+from pyJoules.device import DeviceFactory
+from pyJoules.energy_meter import EnergyMeter
+from pyJoules.handler.pandas_handler import PandasHandler
+
+log = logging.getLogger(__name__)
+
+
+class EnergyContext:
+
+    def __init__(self, experiment: Run, init_tag='init', enable_tracking=False):
+        self.experiment = experiment
+        self.init_tag = init_tag
+        self.handler = PandasHandler()
+        self.enable_tracking = enable_tracking
+
+        self.readable_devices = []
+
+        for device in DeviceFactory.create_devices():
+            try:
+                device.get_energy()
+                log.info(f'Energy consumption monitor available for device: {device}')
+                self.readable_devices.append(device)
+            except OSError:
+                log.warning(f'Cannot read energy consumption from device {device}')
+
+        self.energy_meter = EnergyMeter(self.readable_devices)
+
+    def __enter__(self) -> EnergyMeter:
+        self.energy_meter.start(self.init_tag)
+        return self.energy_meter
+
+    def __exit__(self, type, value, traceback):
+        self.energy_meter.stop()
+
+        if self.enable_tracking and len(self.readable_devices) > 0:
+            self.handler.process(self.energy_meter.get_trace())
+            energy_trace_df = self.handler.get_dataframe()
+
+            with TemporaryDirectory() as temp_dir:
+                energy_trace_df.to_csv(f'{temp_dir}/df.csv', index=False)
+                self.experiment['monitoring/energy'].upload(f'{temp_dir}/df.csv')
+                self.experiment.sync()
+        else:
+            log.warning(f'No readable devices to publish energy consumption data')
--- lulc/train.py
+++ lulc/train.py
@@ -22,6 +22,7 @@ from lulc.data.tx.array import Normalize, Stack, ReclassifyMerge, NanToNum
 from lulc.model.model import SegformerModule
 from model.ops.registry import NeptuneModelRegistry
 from ops.imagery_store_operator import resolve_imagery_store
+from monitoring.energy import EnergyContext
 
 log = logging.getLogger(__name__)
 
@@ -54,83 +55,88 @@ def train(cfg: DictConfig) -> None:
     log.info(f'Configuring remote sensing imagery store: {cfg.imagery.operator}')
     imagery_store = resolve_imagery_store(cfg.imagery, cache_dir=Path(cfg.cache.dir))
 
-    log.info(f'Initializing dataset (area: {cfg.data.descriptor.area}, label: {cfg.data.descriptor.labels})')
+    with EnergyContext(neptune_logger.experiment) as energy_context:
 
-    dataset = AreaDataset(
-        area_descriptor_ver=cfg.data.descriptor.area,
-        label_descriptor_ver=cfg.data.descriptor.labels,
-        imagery_store=imagery_store,
-        data_dir=Path(cfg.data.dir),
-        cache_dir=Path(cfg.cache.dir),
-        deterministic_tx=transforms.Compose([
-            NanToNum(layers=['s1.tif', 's2.tif']),
-            Stack(),
-            ReclassifyMerge(),
-            Normalize(mean=cfg.data.normalize.mean, std=cfg.data.normalize.std)
-        ])
-    )
+        log.info(f'Initializing dataset (area: {cfg.data.descriptor.area}, label: {cfg.data.descriptor.labels})')
 
-    if dataset.item_cache.exists():
-        log.info('Refreshing item intermediate cache (if exists)')
-        rmtree(str(dataset.item_cache))
+        dataset = AreaDataset(
+            area_descriptor_ver=cfg.data.descriptor.area,
+            label_descriptor_ver=cfg.data.descriptor.labels,
+            imagery_store=imagery_store,
+            data_dir=Path(cfg.data.dir),
+            cache_dir=Path(cfg.cache.dir),
+            deterministic_tx=transforms.Compose([
+                NanToNum(layers=['s1.tif', 's2.tif']),
+                Stack(),
+                ReclassifyMerge(),
+                Normalize(mean=cfg.data.normalize.mean, std=cfg.data.normalize.std)
+            ])
+        )
 
-    train_size = int(cfg.data.train_frac * len(dataset))
-    test_size = int(cfg.data.test_frac * len(dataset))
-    val_size = len(dataset) - (train_size + test_size)
+        if dataset.item_cache.exists():
+            log.info('Refreshing item intermediate cache (if exists)')
+            rmtree(str(dataset.item_cache))
 
-    log.info(f'Performing random dataset split (train: {train_size}, val: {val_size}, test: {test_size})')
-    train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
+        train_size = int(cfg.data.train_frac * len(dataset))
+        test_size = int(cfg.data.test_frac * len(dataset))
+        val_size = len(dataset) - (train_size + test_size)
 
-    log.info(f'Attaching random transformations to the training dataset: {list(cfg.model.augment.keys())}')
-    train_dataset.dataset.random_tx = build_random_tx(cfg.model.augment)
+        log.info(f'Performing random dataset split (train: {train_size}, val: {val_size}, test: {test_size})')
+        train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
 
-    log.info('Configuring data loaders')
-    loader_p = partial(DataLoader, batch_size=cfg.model.batch_size,
-                       pin_memory=True,
-                       num_workers=cfg.model.workers,
-                       persistent_workers=True)
-    train_loader = loader_p(dataset=train_dataset, shuffle=True, collate_fn=random_crop_collate_fn(cfg.data.crop.height, cfg.data.crop.width))
-    val_loader = loader_p(dataset=val_dataset, collate_fn=center_crop_collate_fn(cfg.data.crop.height, cfg.data.crop.width))
-    test_loader = loader_p(dataset=test_dataset, collate_fn=center_crop_collate_fn(cfg.data.crop.height, cfg.data.crop.width))
+        log.info(f'Attaching random transformations to the training dataset: {list(cfg.model.augment.keys())}')
+        train_dataset.dataset.random_tx = build_random_tx(cfg.model.augment)
 
-    log.info(f'Creating a model ({cfg.model.variant})')
-    params = dict(
-        num_channels=cfg.model.num_channels,
-        labels=dataset.labels,
-        variant=cfg.model.variant,
-        lr=cfg.model.lr,
-        device=device,
-        class_weights=cfg.data.class_weights,
-        color_codes=dataset.color_codes,
-        max_image_samples=cfg.model.max_image_samples
-    )
-    model = SegformerModule(**params)
-    log.info(f'Training model for {cfg.model.max_epochs} epochs')
+        log.info('Configuring data loaders')
+        loader_p = partial(DataLoader, batch_size=cfg.model.batch_size,
+                           pin_memory=True,
+                           num_workers=cfg.model.workers,
+                           persistent_workers=True)
+        train_loader = loader_p(dataset=train_dataset, shuffle=True, collate_fn=random_crop_collate_fn(cfg.data.crop.height, cfg.data.crop.width))
+        val_loader = loader_p(dataset=val_dataset, collate_fn=center_crop_collate_fn(cfg.data.crop.height, cfg.data.crop.width))
+        test_loader = loader_p(dataset=test_dataset, collate_fn=center_crop_collate_fn(cfg.data.crop.height, cfg.data.crop.width))
 
-    early_stopping = EarlyStopping(monitor='val/epoch/loss', patience=cfg.model.early_stopping.patience)
-    model_checkpoint = ModelCheckpoint(dirpath=f'{output_dir}/checkpoints', save_top_k=2, monitor='val/epoch/loss')
+        log.info(f'Creating a model ({cfg.model.variant})')
+        params = dict(
+            num_channels=cfg.model.num_channels,
+            labels=dataset.labels,
+            variant=cfg.model.variant,
+            lr=cfg.model.lr,
+            device=device,
+            class_weights=cfg.data.class_weights,
+            color_codes=dataset.color_codes,
+            max_image_samples=cfg.model.max_image_samples
+        )
+        model = SegformerModule(**params)
+        log.info(f'Training model for {cfg.model.max_epochs} epochs')
+        energy_context.record('train')
 
-    trainer = pl.Trainer(logger=neptune_logger,
-                         max_epochs=cfg.model.max_epochs,
-                         callbacks=[early_stopping, model_checkpoint],
-                         log_every_n_steps=cfg.model.log_every_n_steps,
-                         gradient_clip_val=cfg.model.gradient_clip_val,
-                         deterministic=cfg.environment.deterministic,
-                         profiler=cfg.environment.profiler)
-    trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
+        early_stopping = EarlyStopping(monitor='val/epoch/loss', patience=cfg.model.early_stopping.patience)
+        model_checkpoint = ModelCheckpoint(dirpath=f'{output_dir}/checkpoints', save_top_k=2, monitor='val/epoch/loss')
 
-    log.info(f'Checking-out best model: {model_checkpoint.best_model_path}')
-    model = SegformerModule.load_from_checkpoint(model_checkpoint.best_model_path, **params)
+        trainer = pl.Trainer(logger=neptune_logger,
+                             max_epochs=cfg.model.max_epochs,
+                             callbacks=[early_stopping, model_checkpoint],
+                             log_every_n_steps=cfg.model.log_every_n_steps,
+                             gradient_clip_val=cfg.model.gradient_clip_val,
+                             deterministic=cfg.environment.deterministic,
+                             profiler=cfg.environment.profiler)
+        trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
-    log.info('Performing model evaluation')
-    trainer.test(model=model, dataloaders=test_loader)
+        log.info(f'Checking-out best model: {model_checkpoint.best_model_path}')
+        model = SegformerModule.load_from_checkpoint(model_checkpoint.best_model_path, **params)
 
-    log.info(f'Model training completed: {run_name}')
-    registry = NeptuneModelRegistry(model_key=cfg.neptune.model.key,
-                                    project=cfg.neptune.project,
-                                    api_key=cfg.neptune.api_token,
-                                    cache_dir=Path(cfg.cache.dir))
-    registry.register_version(model, run_name, neptune_logger.experiment.get_url())
+        log.info('Performing model evaluation')
+        energy_context.record('test')
+        trainer.test(model=model, dataloaders=test_loader)
+
+        log.info(f'Model training completed: {run_name}')
+        registry = NeptuneModelRegistry(model_key=cfg.neptune.model.key,
+                                        project=cfg.neptune.project,
+                                        api_key=cfg.neptune.api_token,
+                                        cache_dir=Path(cfg.cache.dir))
+        energy_context.record('register')
+        registry.register_version(model, run_name, neptune_logger.experiment.get_url())
 
 
 if __name__ == "__main__":
