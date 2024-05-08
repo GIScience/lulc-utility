5d9e1abd6e9c5067de911f3d0b4260541ef12768
Maciej Adamiak <maciej.adamiak@heigit.org>
2024-05-08 11:14:02 +0000

    feat: train LULC model for Dresden

--- lulc/data/collate.py
+++ lulc/data/collate.py
@@ -1,23 +1,25 @@
+from functools import partial
+
 from torch.utils.data import default_collate
 
 from lulc.data.tx.tensor import RandomCrop, CenterCrop
 
 
 def random_crop_collate_fn(crop_height: int, crop_width):
+    return partial(random_crop_collate, crop_height, crop_width)
+
+
+def random_crop_collate(crop_height, crop_width, batch):
     crop = RandomCrop(crop_height, crop_width)
-
-    def random_crop_collate(batch):
-        batch = [crop(sample) for sample in batch]
-        return default_collate(batch)
-
-    return random_crop_collate
+    batch = [crop(sample) for sample in batch]
+    return default_collate(batch)
 
 
 def center_crop_collate_fn(crop_height: int, crop_width: int):
+    return partial(center_crop_collate, crop_height, crop_width)
+
+
+def center_crop_collate(crop_height, crop_width, batch):
     crop = CenterCrop(crop_height, crop_width)
-
-    def random_crop_collate(batch):
-        batch = [crop(sample) for sample in batch]
-        return default_collate(batch)
-
-    return random_crop_collate
+    batch = [crop(sample) for sample in batch]
+    return default_collate(batch)
