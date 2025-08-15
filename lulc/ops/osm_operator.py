bea15e41dfcb81b5a3869b0060ac13cebcbc4c09
Danielle Gatland <danielle.gatland@heigit.org>
2025-08-15 15:37:27 +0200

    doc: add docstrings to OhsomeOps

--- lulc/ops/osm_operator.py
+++ lulc/ops/osm_operator.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 import rasterio
 import shapely
+import xarray
 from geocube.api.core import make_geocube
 from hydra.core.hydra_config import HydraConfig
 from ohsome import OhsomeClient
@@ -34,6 +35,15 @@ class OhsomeOps:
         osm_lulc_mapping: Dict[str, LabelDescriptor],
         target_size: Tuple[int, int],
     ) -> Dict[str, np.ndarray]:
+        """
+        Query the ground truth class labels from OSM features and return a dictionary containing the rasterised labels
+        for each class.
+
+        :param area_coords: bounding box coordinates to generate labels for
+        :param time: the datetime to query OSM data for (only valid OSM features at this time will be included)
+        :param osm_lulc_mapping: a dictionary with keys as class labels and values as ohsome filters for OSM features
+        :param target_size: the target dimensions of the rasterised labels
+        """
         result = {}
         height, width = target_size
         bbox = ','.join(map(str, area_coords))
@@ -59,6 +69,17 @@ class OhsomeOps:
         width: int,
         osm_lulc_mapping: Tuple[str, LabelDescriptor],
     ) -> Tuple[str, np.ndarray]:
+        """
+        Query OSM for the features within the bbox that satisfy the filters defined in the osm_lulc_mapping.
+
+        :param bbox: bounding box coordinates to get labels for
+        :param bbox_id: a unique id to save the results to
+        :param time: the datetime to query OSM data for (only valid OSM features at this time will be included)
+        :param utm: the utm code for the provided bbox
+        :param height: the target height for the rasterised labels
+        :param width: the target width for the rasterised labels
+        :param osm_lulc_mapping: the class name and the ohsome filter to be queried for that class
+        """
         label_name, label = osm_lulc_mapping
         data_folder = self.cache_dir / label_name
         data_folder.mkdir(parents=True, exist_ok=True)
@@ -97,7 +118,16 @@ class OhsomeOps:
         hex_string = hashlib.md5(text.encode('UTF-8')).hexdigest()
         return uuid.UUID(hex=hex_string)
 
-    def __computer_raster(self, vector_data, utm):
+    def __computer_raster(self, vector_data: gpd.GeoDataFrame, utm: str) -> xarray.Dataset:
+        """
+        Sort the `vector_data` and then rasterise it with geocube, which takes the first value from the `vector_data`,
+        ignoring duplicates if they exist.
+
+        :param vector_data: a GeoDataFrame containing the queried OSM features (for one class), plus a 'default'
+        geometry representing the full bounds, with a column `value` which is 1 for every feature and 0 for the
+        'default' feature.
+        :param utm: the utm code for the provided bbox
+        """
         sorted_desc_areas_idx = vector_data.copy().to_crs(utm).geometry.area.argsort()[::-1]
 
         return make_geocube(
